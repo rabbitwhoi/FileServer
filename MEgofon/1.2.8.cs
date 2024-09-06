f6c5e1f91f1e455e2f756bc83c0858cb98671a13
rabbitwhoi <151567849+rabbitwhoi@users.noreply.github.com>
2024-09-06 18:52:52 +0300

    Add files via upload

---
+++ MEgofon/1.2.8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace program 
+{
+    class Program
+    {
+        static void Main()
+        {
+            double sk = 121;
+            //Сторона квадрата
+            double ak = Math.Sqrt(sk);
+            //Ширина прямоугольника
+            double b = ak - 0.2;
+            //Длина прямоугольника
+            double a = 2 * ak;
+            //Площадь прямоугольника
+            double s = a * b;
+
+            Console.WriteLine("Площадь прямоугольника: " + s);
+
+            
+        }
+
+
+    }
+
+}
