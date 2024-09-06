e6613391446f8f9bfb43bd35499866bb6c222b94
rabbitwhoi <151567849+rabbitwhoi@users.noreply.github.com>
2024-09-06 12:37:18 +0300

    Add files via upload

---
+++ MEgofon/1.2.6.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите периметр: ");
+string input = Console.ReadLine();
+
+double p = Convert.ToInt16(input);
+
+
+double s1 = (p / 4);
+
+double s = s1 * s1;
+//double result = Math.Pow(s1, 2);
+
+Console.WriteLine("Площадь ровна: " + s);
