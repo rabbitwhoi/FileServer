7c8f54ddf02af4ab782c2a6f340ac814d8d33313
rabbitwhoi <151567849+rabbitwhoi@users.noreply.github.com>
2024-09-06 18:49:50 +0300

    Add files via upload

---
+++ MEgofon/1.2.8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace program 
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            double a = 25;
+            double b = 3;
+
+            // Скорость катера по течению 
+            double v_po_teceniu = a + b;
+            //Скорость катера против течения
+            double v_protiv = a - b;
+            //Отношение
+            double result = v_po_teceniu / v_protiv;
+            Console.WriteLine("Скорость катера по течению больше скорости против течения в " + result + " раза");
+
+            
+        }
+
+
+    }
+
+}
